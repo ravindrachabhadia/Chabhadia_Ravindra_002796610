b6a13ec6310321f2a2feb9b7390e39cef7fae840
ravindrachabhadia <114537139+ravindrachabhadia@users.noreply.github.com>
2022-10-01 01:35:34 -0400

    Add files via upload

---
+++ Assignment-1/src/model/EmployeeHistory.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Chabhadia
+ */
+public class EmployeeHistory {
+    private ArrayList<Manager>history;
+    
+    public EmployeeHistory(){
+    this.history= new ArrayList<Manager>();
+    
+    }
+
+    public ArrayList<Manager> getHistory() {
+        return history;
+    }
+
+    public void setHistory(ArrayList<Manager> history) {
+        this.history = history;
+    }
+
+    public Manager addNewManager() {
+        
+        Manager newManagers= new Manager();
+        history.add(newManagers);
+        return newManagers;
+        
+       }
+
+    public Manager addNewManagers() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+     
+    
+}
---
+++ Assignment-1/src/model/Manager.java
@@ -0,0 +1,112 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author Chabhadia
+ */
+public class Manager {
+   private String name;
+   private String employeeId;
+   private int age;
+   private String gender;
+   private String startDate;
+   private int level;
+   private String teamInfo;
+   private String positionTitle;
+   private int Number;
+   private String EmailID;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+  
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(String startDate) {
+        this.startDate = startDate;
+    }
+
+    
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    
+
+    public String getTeamInfo() {
+        return teamInfo;
+    }
+
+    public void setTeamInfo(String teamInfo) {
+        this.teamInfo = teamInfo;
+    }
+
+    public String getPositionTitle() {
+        return positionTitle;
+    }
+
+    public void setPositionTitle(String positionTitle) {
+        this.positionTitle = positionTitle;
+    }
+
+    public String getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(String employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public int getNumber() {
+        return Number;
+    }
+
+    public void setNumber(int Number) {
+        this.Number = Number;
+    }
+
+    public String getEmailID() {
+        return EmailID;
+    }
+
+    public void setEmailID(String EmailID) {
+        this.EmailID = EmailID;
+    }
+
+    
+   
+    
+}
